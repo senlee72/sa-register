66571fecb94693153c508c0dc4a6b17ec0a154bd
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 19:01:10 -0400

    added form

--- sa-regplugin/includes/regform.php
+++ sa-regplugin/includes/regform.php
@@ -7,7 +7,7 @@ function create_reg_form() {
 
 function create_rest_endpoint() {
     register_rest_route( 'v1/regformapi', 'submit', array(
-        'method' => 'POST',
+        'methods' => 'POST',
         'callback' => 'handle_form_submit'
 ));
 }
