92ffd95ccaa562431435c6651429ab8882ddcfd9
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 13:00:05 -0400

    added composer

--- sa-regplugin/reg.php
+++ sa-regplugin/reg.php
@@ -5,4 +5,18 @@
  * Version: 1.0.0
  * Text Domain: options-plugin
  */
+
+ if (!defined('ABSPATH')) {
+    die('You cannot be here ');    
+ }
+
+ if (!class_exists('RegEnroll')){
+ class RegEnroll {
+    public function __construct() {
+        require_once( plugin_dir_path(__FILE__).'/vendor/autoload.php');
+    }
+
+ }
+ new RegEnroll;
+}
 ?>
