45db68f98e16279e4c885577b6920b52befcd7d4
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 18:47:32 -0400

    added form

--- sa-regplugin/includes/forms/regdetails.php
+++ sa-regplugin/includes/forms/regdetails.php
@@ -4,7 +4,7 @@
     <button type="submit">Submit Form</button>
 </form>
 <script>
-    jquery(document).ready(function($) {
+    jQuery(document).ready(function($) {
         $("#regFormPage").submit( function (e) {
                  alert('Form submited');
                  e.preventDefault();
