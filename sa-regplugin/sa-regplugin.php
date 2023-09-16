e532ad7afd8fda1aa68c25a6d78a5615442252ea
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 16:43:13 -0400

    add log

--- sa-regplugin/sa-regplugin.php
+++ sa-regplugin/sa-regplugin.php
@@ -13,8 +13,11 @@
  if (!class_exists('RegEnroll')){
  class RegEnroll {
     public function __construct() {
-        define ('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+        define ('MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
         require_once( MY_PLUGIN_PATH.'/vendor/autoload.php');
+        $pluginlog = plugin_dir_path(__FILE__).'debug.log';
+        $message = 'CATCHING ERROR'.PHP_EOL;
+        error_log($message, 3, $pluginlog);
     }
 
     public function initialize() {
