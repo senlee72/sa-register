8a82df6492d4ea877f65084c497748b0a91c2113
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 18:38:13 -0400

    added form

--- sa-regplugin/includes/forms/regdetails.php
+++ sa-regplugin/includes/forms/regdetails.php
@@ -1,4 +1,15 @@
-<form>
-<label>Name</label>
-<input type="text" name="name"> <br/>
+<form id="regFormPage">
+    <label>Name</label>
+    <input type="text" name="name"> <br/>
+    <button type="submit">Submit Form</button>
 </form>
+<script>
+    jquery(d).ready(function($) {
+        $("#regFormPage").submit(
+            function (e) {
+                 e.preventDefault();
+                 alert('Form submited');
+            }
+        )
+    }); 
+</script>
--- sa-regplugin/includes/regform.php
+++ sa-regplugin/includes/regform.php
@@ -6,5 +6,9 @@ function create_reg_form() {
 }
 
 function create_rest_endpoint() {
-
+    register_rest_route( 'v1/regformapi', 'submit', array(
+        'method' => 'POST',
+        'callback' => 'handle_form_submit'
+));
+    
 }
