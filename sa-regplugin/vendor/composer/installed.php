2e2bd598b154abfcf06ad2671d8c091de6469b79
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 13:30:53 -0400

    composer install

---
+++ sa-regplugin/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-plugindev',
+        'version' => 'dev-plugindev',
+        'reference' => '82d58bdd4305a2530b3cb4eb263d9e7f712a8bb1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-plugindev',
+            'version' => 'dev-plugindev',
+            'reference' => '82d58bdd4305a2530b3cb4eb263d9e7f712a8bb1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '47f1538cca6cd2860d1b37a040a42c59eb798d76',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
