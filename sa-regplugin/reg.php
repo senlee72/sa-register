0b4ef7e6954bcf903fdbce1fbea2d3a12fd714d0
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 13:54:24 -0400

    composer carbon fields

--- sa-regplugin/reg.php
+++ sa-regplugin/reg.php
@@ -13,7 +13,13 @@
  if (!class_exists('RegEnroll')){
  class RegEnroll {
     public function __construct() {
-        require_once( plugin_dir_path(__FILE__).'/vendor/autoload.php');
+        define ('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+        require_once( MY_PLUGIN_PATH.'/vendor/autoload.php');
+    }
+
+    public function initialize() {
+        include_once MY_PLUGIN_PATH.'/includes/utilities.php';
+        include_once MY_PLUGIN_PATH.'/includes/options-page.php';
     }
 
  }
