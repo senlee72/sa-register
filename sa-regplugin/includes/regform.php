5ee42affaf66a4b0ba0ed1b4ab723452b0edb5ba
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 18:28:28 -0400

    added regorm

--- sa-regplugin/includes/regform.php
+++ sa-regplugin/includes/regform.php
@@ -2,9 +2,9 @@
 add_shortcode( 'regform', 'create_reg_form' );
 
 function create_reg_form() {
-    include MY_PLUGIN_PATH . 'includes/firns.regdetails.php';
+    include MY_PLUGIN_PATH . 'includes/forms/regdetails.php';
 }
 
 function create_rest_endpoint() {
-    
+
 }
