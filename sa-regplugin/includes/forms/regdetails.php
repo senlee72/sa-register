8f96f1e04664982b439b26781618682446d90721
senlee72 <sentheesh.lingam@gmail.com>
2023-09-16 19:35:59 -0400

    added form

--- sa-regplugin/includes/forms/regdetails.php
+++ sa-regplugin/includes/forms/regdetails.php
@@ -1,3 +1,6 @@
+<div id="formsuccess" style="background: green; color:#fff"/>
+<div id="formerror" style="background:red; color:#fff"/>
+
 <form id="regFormPage">
     <?php wp_nonce_field('wp_rest');?>
     <label>Name</label>
@@ -12,7 +15,13 @@
             $.ajax({
                 type: "POST",
                 url: "<?php echo get_rest_url( null, "v1/regformapi/submit" );?>",
-                data: form.serialize()
+                data: form.serialize(),
+                success: function() {
+                    $("#formsuccess").html("Your message was sent").fadeIn();
+                },
+                error: function() {
+                    $("#formerror").html("There was an error").fadeIn();
+                }
             });
         })
     }); 
